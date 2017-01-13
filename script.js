a96b0b0c0dcfd407f129fa9855ac222fafe33054
Riza Fahmi <rizafahmi@gmail.com>
2017-01-13 10:42:00 +0700

    Scales basic

--- script.js
+++ script.js
@@ -4,8 +4,13 @@ let dataset = [5, 10, 15, 20, 25]
 const svg = d3.select('#chartArea').append('svg')
   .attr('width', 400)
   .attr('height', 300)
+  .style('background', '#cacaca')
 
 const multiplier = 8
+
+const yScale = d3.scaleLinear()
+  .domain([0, 25])
+  .range([0, 300])
 
 svg.selectAll('rect')
   .data(dataset)
@@ -16,9 +21,9 @@ svg.selectAll('rect')
     return i * 22
   })
   .attr('y', (d) => {
-    return 300 - d * multiplier
+    return 300 - yScale(d)
   })
   .attr('width', 20)
   .attr('height', (d) => {
-    return d * multiplier
+    return yScale(d)
   })
