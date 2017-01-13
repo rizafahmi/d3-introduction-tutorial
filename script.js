318ba1eaaf3138b22314f1924a76e9eb395b1058
Riza Fahmi <rizafahmi@gmail.com>
2017-01-13 10:14:45 +0700

    Use SVG

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 /* global d3 */
 let dataset = [5, 10, 15, 20, 25]
 
-d3.select('body').selectAll('div')
-    .data(dataset)
-    .enter()
-    .append('div')
-    .attr('class', 'bar')
-    .style('height', function (d) {
-      return d * 5 + 'px'
-    })
+const svg = d3.select('#chartArea').append('svg')
+  .attr('width', 400)
+  .attr('height', 300)
+
+const multiplier = 8
+
+svg.selectAll('rect')
+  .data(dataset)
+  .enter()
+  .append('rect')
+  .attr('class', 'bar')
+  .attr('x', (d, i) => {
+    return i * 22
+  })
+  .attr('y', (d) => {
+    return 300 - d * multiplier
+  })
+  .attr('width', 20)
+  .attr('height', (d) => {
+    return d * multiplier
+  })
